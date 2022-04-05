2b7c3ad966b6c6e41137ef6ad21a8a14c99be831
Dodsie <baileydods@gmail.com>
2022-04-04 21:00:47 -0600

    Quering for Joining tables have been added under specific conditions.

---
+++ 2_queries_joins/cohort_total_duration.sql
@@ -0,0 +1,4 @@
+SELECT sum(assignment_submissions.duration) as total_cohort_duration
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id 
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE cohorts.name = 'FEB12';
