6b6ca8d071777e4e5235107ccee12e9f5c62b129
Dodsie <baileydods@gmail.com>
2022-04-06 21:41:37 -0600

    Santize inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,18 @@ const pool = new Pool({
 
 pool.connect();
 
+const cohortName = [`${process.argv[2]}`];
+
 pool.query(
   `SELECT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
+  WHERE cohorts.name = $1
   GROUP BY teacher, cohort
   ORDER BY teacher;
-`).then(res => {
+`, cohortName).then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   });
